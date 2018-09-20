20c8eb0c36ddd769ba9876e3c9af2fdcb70a3b80
CheloGE <chelo_ge@hotmail.com>
2018-09-19 20:11:56 -0500

    adding rbpi3 motors package

---
+++ src/packages/rbpi3_motor_controller/src/motors_subscriber.cpp
@@ -0,0 +1,21 @@
+#include "ros/ros.h"
+#include "rbpi3_motor_controller/motors_signal.h"
+
+void motors_callback(const rbpi3_motor_controller::motors_signal::ConstPtr& motors_signal){
+    ROS_INFO("motor left :%i  motor right: %i",motors_signal->motor_left,motors_signal->motor_right);
+}
+
+main(int argc, char **argv)
+{
+    // initialize ros node
+    ros::init(argc,argv, "motors_subscriber");
+    // create an object of type nodehandle
+    ros::NodeHandle node_object;
+    // create a publisher object targeting a topic
+    ros::Subscriber msg_publisher = \
+    node_object.subscribe<rbpi3_motor_controller::motors_signal>("/motors_topic",10,motors_callback);
+    // spinning for callback
+    ros::spin();
+    
+    return 0;
+}
