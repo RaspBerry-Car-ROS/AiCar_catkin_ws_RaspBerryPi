22e643a84d425e3e4e9a844025f8d40048509ad0
CheloGE <chelo_ge@hotmail.com>
2018-09-21 13:55:23 -0500

    added logic for controlling car

--- src/packages/rbpi3_motor_controller/src/motors_subscriber.cpp
+++ src/packages/rbpi3_motor_controller/src/motors_subscriber.cpp
@@ -3,7 +3,42 @@
 
 #include "wiringPi.h"
 
+// Defining Pins
+#define LEFT_FORWARD 0
+#define LEFT_BACKWARD 2
+#define RIGHT_FORWARD 4
+#define RIGHT_BACKWARD 5
+
+
+/*************************************************************************/
+/************************* MOTORS CALLBACK *******************************/
+/*************************************************************************/
 void motors_callback(const rbpi3_motor_controller::motors_signal::ConstPtr& motors_signal){
+    
+    if (motors_signal->motor_left == 1) {
+        digitalWrite (LEFT_BACKWARD, LOW) ;
+        digitalWrite (LEFT_FORWARD, HIGH) ;
+    }else if(motors_signal->motor_left == -1)
+    {
+        digitalWrite (LEFT_FORWARD, LOW) ;
+        digitalWrite (LEFT_BACKWARD, HIGH) ;
+    }
+
+    if (motors_signal->motor_right == 1) {
+        digitalWrite (RIGHT_BACKWARD, LOW) ;
+        digitalWrite (RIGHT_FORWARD, HIGH) ;
+    }else if(motors_signal->motor_right == -1)
+    {
+        digitalWrite (RIGHT_BACKWARD, LOW) ;
+        digitalWrite (RIGHT_FORWARD, HIGH) ;   
+    }
+
+    if(motors_signal->motor_right == 0 and motors_signal->motor_left == 0)
+    {
+        digitalWrite (LEFT_FORWARD, LOW) ;
+        digitalWrite (LEFT_BACKWARD, LOW) ;
+    }
+    
     ROS_INFO("motor left :%i  motor right: %i",motors_signal->motor_left,motors_signal->motor_right);
 }
 
@@ -11,8 +46,21 @@ main(int argc, char **argv)
 {
     // initialize ros node
     ros::init(argc,argv, "motors_subscriber");
-    // testing wiringPi
+    /************************************/
+    /********* wiringPi setup ***********/
+    /************************************/
+    
+    // setting all pins as wiringPi
     wiringPiSetup();
+    // setting required pins as output
+    pinMode(LEFT_FORWARD, OUTPUT);
+    pinMode(LEFT_BACKWARD, OUTPUT);
+    pinMode(RIGHT_FORWARD, OUTPUT);
+    pinMode(RIGHT_BACKWARD, OUTPUT);
+
+    /************************************/
+    /*********** ROS setup **************/
+    /************************************/
     // create an object of type nodehandle
     ros::NodeHandle node_object;
     // create a publisher object targeting a topic
