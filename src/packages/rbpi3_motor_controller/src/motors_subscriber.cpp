fe18cdb5a354baf22804fc2c559778cab8fc1d13
CheloGE <chelo_ge@hotmail.com>
2018-09-21 12:38:20 -0500

    wiringPi lirary implemented

--- src/packages/rbpi3_motor_controller/src/motors_subscriber.cpp
+++ src/packages/rbpi3_motor_controller/src/motors_subscriber.cpp
@@ -1,6 +1,8 @@
 #include "ros/ros.h"
 #include "rbpi3_motor_controller/motors_signal.h"
 
+#include "wiringPi.h"
+
 void motors_callback(const rbpi3_motor_controller::motors_signal::ConstPtr& motors_signal){
     ROS_INFO("motor left :%i  motor right: %i",motors_signal->motor_left,motors_signal->motor_right);
 }
@@ -9,10 +11,12 @@ main(int argc, char **argv)
 {
     // initialize ros node
     ros::init(argc,argv, "motors_subscriber");
+    // testing wiringPi
+    wiringPiSetup();
     // create an object of type nodehandle
     ros::NodeHandle node_object;
     // create a publisher object targeting a topic
-    ros::Subscriber msg_publisher = \
+    ros::Subscriber msg_subscriber = \
     node_object.subscribe<rbpi3_motor_controller::motors_signal>("/motors_topic",10,motors_callback);
     // spinning for callback
     ros::spin();
